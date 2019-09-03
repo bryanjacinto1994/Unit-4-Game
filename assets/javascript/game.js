2fe6bdee6fc7ffd121514f215133e94a3489d266
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-09-03 15:58:53 -0700

    Added a pseudocode instructions to the game.js to make it easier to execute a coding plan

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+//Create a function that will run the javascript file as soon as the browser loads.
+
+
+//Create Variables that has "FOUR" items and variables that represents the scores.
+
+
+//Create a function that will randomize the numbers you need to reach.
+//Make sure that the value is between 19 - 120.
+
+//Create another function that will randomize each of the four items to have different values.
+//Make sure that the value is between 1 - 12.
+
+
+//Create a function to have the whole game reset when it is finished.
+
+
+//Create a function if a user wins: it will say "You Won!", and the win score will increment by 1 and the game resets.
+//Create a function if a user loses: it will say "Game Over!", and the lose score will increment by 1 and the game resets.
+
+
+//Create a "click" function for each items and sum the user's score with each of those items.
+//Make an if and else statement for when the user wins or when the user loses the game.
+
+
